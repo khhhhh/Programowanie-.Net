758087481c233cc351d2ca1c07e313fff031e6ba
aleh <oleg.hutsko@gmail.com>
2022-03-15 12:57:16 +0100

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Cokolwiek.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Encodings.Web;
+
+namespace Cokolwiek.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        // https://localhost:7001/Home/Greeting/5?name=Aleh 
+        // https://localhost:7001/Home/Greeting?name=Aleh&ID=5
+        public IActionResult Greeting(string name, int ID = 1)
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cokolwiek.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
